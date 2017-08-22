3b11beb4cfe6180f9e5390bac597c8bdba6673ac
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-22 19:00:35 +0200

    split private room price

--- db/seeds.rb
+++ db/seeds.rb
@@ -108,7 +108,7 @@ surfcamps.each do |surfcamp|
         if room.capacity == 1
           room.price_per_night = rand(70..99)
         else
-          room.price_per_night = rand(100..120)
+          room.price_per_night = rand(50..60)
         end
       end
     room.surfcamp_id = surfcamp.id
