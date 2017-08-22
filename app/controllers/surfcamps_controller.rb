5e186a9aacd1b493a21f0a01d7fd7e8f11ee518f
Arthur Allamand <a.allamand@gmail.com>
2017-08-22 17:38:14 +0200

    remove some code that wasn't useful for today --> controller new & create

--- app/controllers/surfcamps_controller.rb
+++ app/controllers/surfcamps_controller.rb
@@ -17,19 +17,6 @@ class SurfcampsController < ApplicationController
   def show
   end
 
-  def new
-    @surfcamp = Surfcamp.new
-  end
-
-  def create
-    @surfcamp = Surfcamp.new(set_params)
-     if @surfcamp
-      redirect_to surfcamps_path(@surfcamp)
-    else
-      render :new
-    end
-  end
-
   private
 
   def set_surfcamp
