b17f4a2ac347852b19b06192f0becf8ca6aaba25
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-23 18:40:16 +0200

    show logic for displaying all rooms on book form, but only last room going through params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,48 +3,48 @@ class BookingsController < ApplicationController
     @booking = Booking.new(set_params)
     @booking.surfcamp_id = params[:surfcamp_id]
     @booking.save
-    #create occupancy for dorm for this booking
-    @occupancy = Occupancy.new(set_occupancy_params)
-    @occupancy.booking_id = @booking.id
-    room = Room.find(@occupancy.room_id)
-    nb_days = (@booking.ends_at - @booking.starts_at)/86400
-    # discount for room
-    room.discounts.first.blank? ? discount = nil : discount = room.discounts.first
+    # create occupancy for dorm for this booking
+    # TODO: iterate on array of params of room_id & pax
+      @occupancy = Occupancy.new(set_occupancy_params)
+      @occupancy.booking_id = @booking.id
+      room = Room.find(@occupancy.room_id)
+      nb_days = (@booking.ends_at - @booking.starts_at)/86400
+      # discount for room
+      room.discounts.first.blank? ? discount = nil : discount = room.discounts.first
 
-    # Build array of nights
-    nights = []
-    night = @booking.starts_at.to_date
-    nb_days.to_i.times do |_n|
-      nights << night
-      night += 1
-    end
-    # for each night
-    total_price = 0
-    nights.each do |night|
-      # check if night has a discount
-      if discount.blank?
-        total_price = room.price_per_night * nb_days
-      else
-        # if night has a discount check if nights are inside the discount dates
-        if night >= room.discounts.first.discount_starts_at && night <= room.discounts.first.discount_ends_at
-          night_price = room.price_per_night
-        else
-          # price per night with the discount
-          night_price = discount.discounted_price
-        end
-        # sum each price per night with discount
-        total_price += night_price
+      # Build array of nights
+      nights = []
+      night = @booking.starts_at.to_date
+      nb_days.to_i.times do |_n|
+        nights << night
+        night += 1
       end
-    end
+      # for each night
+      total_price = 0
+      nights.each do |night|
+        # check if night has a discount
+        if discount.blank?
+          total_price = room.price_per_night * nb_days
+        else
+          # if night has a discount check if nights are inside the discount dates
+          if night >= room.discounts.first.discount_starts_at && night <= room.discounts.first.discount_ends_at
+            night_price = @room.price_per_night
+          else
+            # price per night with the discount
+            night_price = discount.discounted_price
+          end
+          # sum each price per night with discount
+          total_price += night_price
+        end
+      end
 
-    price = @occupancy.pax_nb * total_price
+      price = @occupancy.pax_nb * total_price
 
-    @occupancy.price = price
-    @occupancy.save!
+      @occupancy.price = price
+      @occupancy.save!
+    # end of iteration on params of room_id
 
-    # :price, :pax_nb, :booking_id, :room_id
-
-    #redirect to modify
+    # redirect to modify
     redirect_to surfcamps_path
   end
 
