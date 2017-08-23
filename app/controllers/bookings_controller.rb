fb92a02da928ee0fab72bf4ee771b7107e84a7b0
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-23 13:01:04 +0200

    updated form to generate bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,14 @@
+class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(set_params)
+    @booking.save
+    #redirect to modify
+    redirect_to surfcamps_path
+  end
+
+  private
+
+  def set_params
+    params.require(:booking).permit(:starts_at, :ends_at, :status, :user_id, :surfcamp_id)
+  end
+end
