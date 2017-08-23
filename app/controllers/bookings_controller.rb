20b4df78e5777126ddcf626797a9c99cb1d24941
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-23 17:11:59 +0200

    created occupany for 1st room of each surfcamp

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,47 @@ class BookingsController < ApplicationController
     @booking = Booking.new(set_params)
     @booking.surfcamp_id = params[:surfcamp_id]
     @booking.save
+    #create occupancy for dorm for this booking
+    @occupancy = Occupancy.new(set_occupancy_params)
+    @occupancy.booking_id = @booking.id
+    room = Room.find(@occupancy.room_id)
+    nb_days = (@booking.ends_at - @booking.starts_at)/86400
+    # discount for room
+    room.discounts.first.blank? ? discount = nil : discount = room.discounts.first
+
+    # Build array of nights
+    nights = []
+    night = @booking.starts_at.to_date
+    nb_days.to_i.times do |_n|
+      nights << night
+      night += 1
+    end
+    # for each night
+    total_price = 0
+    nights.each do |night|
+      # check if night has a discount
+      if discount.blank?
+        total_price = room.price_per_night * nb_days
+      else
+        # if night has a discount check if nights are inside the discount dates
+        if night >= room.discounts.first.discount_starts_at && night <= room.discounts.first.discount_ends_at
+          night_price = room.price_per_night
+        else
+          # price per night with the discount
+          night_price = discount.discounted_price
+        end
+        # sum each price per night with discount
+        total_price += night_price
+      end
+    end
+
+    price = @occupancy.pax_nb * total_price
+
+    @occupancy.price = price
+    @occupancy.save!
+
+    # :price, :pax_nb, :booking_id, :room_id
+
     #redirect to modify
     redirect_to surfcamps_path
   end
@@ -11,5 +52,9 @@ class BookingsController < ApplicationController
 
   def set_params
     params.require(:booking).permit(:starts_at, :ends_at, :status, :user_id, :surfcamp_id)
+  end
+
+  def set_occupancy_params
+    params.require(:occupancy).permit(:pax_nb, :room_id)
   end
 end
