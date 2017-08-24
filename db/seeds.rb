07dbf52aa21302916ab4c9c8353255f06e4a2a0f
Thibault Jaime <jaime.thibault@gmail.com>
2017-08-24 13:10:03 +0200

    seed uncommented

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,111 +12,111 @@ puts "Destroying Discounts"
 Discount.destroy_all
 puts "Destroying Bookings"
 Booking.destroy_all
-# puts "Destroying Surfcamps"
-# Surfcamp.destroy_all
-# puts "Destroying Users"
-# User.destroy_all
+puts "Destroying Surfcamps"
+Surfcamp.destroy_all
+puts "Destroying Users"
+User.destroy_all
 
 
-# puts "Creating Users"
-# i = 0
-# users = [
-#   "jackie@michel.com",
-#   "michel@jackie.com",
-#   "micheline@jackie.com"
-# ]
-# first_names = [
-#   "Thibault",
-#   "Clemence",
-#   "Dima"
-# ]
+puts "Creating Users"
+i = 0
+users = [
+  "jackie@michel.com",
+  "michel@jackie.com",
+  "micheline@jackie.com"
+]
+first_names = [
+  "Thibault",
+  "Clemence",
+  "Dima"
+]
 
-# 3.times do
-#   user = User.new
-#   user.email = users[i]
-#   user.password = 'password'
-#   user.first_name = first_names[i]
-#   user.last_name = Faker::Name.last_name
-#   urls = [
-#     "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/10997490_907097779334969_1284262528561985815_n.jpg?oh=c3afa5c308ce7405d109656dda4ffd50&oe=5A2F762F",
-#     "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/20799136_274376209715766_7099253975910970765_n.jpg?oh=e84997278e80801d94f81ca917120c19&oe=5A1A935E",
-#     "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/19875558_10155671439642176_1610883172945385131_n.jpg?oh=482cbb47e3786e5c1c813330c294645e&oe=5A16D6B7"
-#   ]
-#   user.facebook_picture_url = urls[i]
-#   user.save!
-#   i += 1
-# end
-# puts "Users created"
+3.times do
+  user = User.new
+  user.email = users[i]
+  user.password = 'password'
+  user.first_name = first_names[i]
+  user.last_name = Faker::Name.last_name
+  urls = [
+    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/10997490_907097779334969_1284262528561985815_n.jpg?oh=c3afa5c308ce7405d109656dda4ffd50&oe=5A2F762F",
+    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/20799136_274376209715766_7099253975910970765_n.jpg?oh=e84997278e80801d94f81ca917120c19&oe=5A1A935E",
+    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/19875558_10155671439642176_1610883172945385131_n.jpg?oh=482cbb47e3786e5c1c813330c294645e&oe=5A16D6B7"
+  ]
+  user.facebook_picture_url = urls[i]
+  user.save!
+  i += 1
+end
+puts "Users created"
 
-# puts "Creating Admin"
-# admin = User.new
-# admin.email = "admin@admin.admin"
-# admin.password = "astrongpassword"
-# admin.first_name = "admin"
-# admin.last_name = "ADMIN"
-# admin.facebook_picture_url = "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/10997490_907097779334969_1284262528561985815_n.jpg?oh=c3afa5c308ce7405d109656dda4ffd50&oe=5A2F762F"
-# admin.admin = true
-# admin.save!
-# puts "Admin created"
+puts "Creating Admin"
+admin = User.new
+admin.email = "admin@admin.admin"
+admin.password = "astrongpassword"
+admin.first_name = "admin"
+admin.last_name = "ADMIN"
+admin.facebook_picture_url = "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/10997490_907097779334969_1284262528561985815_n.jpg?oh=c3afa5c308ce7405d109656dda4ffd50&oe=5A2F762F"
+admin.admin = true
+admin.save!
+puts "Admin created"
 
-# puts "Creating Surfcamps"
-# s = 0
-# # The url we are scrapping
-# url = "https://www.surfholidays.com/property-search?country=all&town=all&checkin=&checkout=&guests=2&suitable_for=surfcamps"
-# base_url = "https://www.surfholidays.com"
-# html_file = open(url).read
-# html_doc = Nokogiri::HTML(html_file)
-# surfcamp_total = html_doc.search(".name-location a").count
-# # We look for all the a in the div that match our criterias
-# html_doc.search(".name-location a").each do |element|
-#   page_url = element.attribute('href').value
-#   complete_url = "#{base_url}#{page_url}"
+puts "Creating Surfcamps"
+s = 0
+# The url we are scrapping
+url = "https://www.surfholidays.com/property-search?country=all&town=all&checkin=&checkout=&guests=2&suitable_for=surfcamps"
+base_url = "https://www.surfholidays.com"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+surfcamp_total = html_doc.search(".name-location a").count
+# We look for all the a in the div that match our criterias
+html_doc.search(".name-location a").each do |element|
+  page_url = element.attribute('href').value
+  complete_url = "#{base_url}#{page_url}"
 
-#   html_file = open(complete_url).read
-#   html_doc = Nokogiri::HTML(html_file)
-#   images_surfcamp = []
+  html_file = open(complete_url).read
+  html_doc = Nokogiri::HTML(html_file)
+  images_surfcamp = []
 
-#   # Initializing instance of surfcamp
-#   surfcamp = Surfcamp.new
+  # Initializing instance of surfcamp
+  surfcamp = Surfcamp.new
 
-#   # We create surfcamp with the data that has been scrapped
-#   html_doc.search("#custom-slider ul li").each do |element|
-#     images_surfcamp << element['style'][/url\((.+)\)/, 1].gsub("'","")
-#   end
+  # We create surfcamp with the data that has been scrapped
+  html_doc.search("#custom-slider ul li").each do |element|
+    images_surfcamp << element['style'][/url\((.+)\)/, 1].gsub("'","")
+  end
 
-#   # creating surfcamp image
-#   surfcamp.photo_url = images_surfcamp[0]
-#   html_doc.search("h1.sh-navy").each do |element|
-#     name = element.text
-#     # creating surfcamp name
-#     surfcamp.name = name
-#   end
+  # creating surfcamp image
+  surfcamp.photo_url = images_surfcamp[0]
+  html_doc.search("h1.sh-navy").each do |element|
+    name = element.text
+    # creating surfcamp name
+    surfcamp.name = name
+  end
 
-#   # creating surfcamp description
-#   surfcamp.description = Faker::Lorem.paragraph
+  # creating surfcamp description
+  surfcamp.description = Faker::Lorem.paragraph
 
-#    html_doc.search("#accom-detail-location").each do |element|
-#     address = element.text.strip
-#     # creating surfcamp address
-#     surfcamp.address = address
-#   end
-#   ratings = []
-#   html_doc.search("p.bolder.sh-orange span.bigger-font18").each do |element|
-#     ratings << element.text.strip
-#     # creating surfcamp rating
-#     surfcamp.rating = ratings[0]
-#   end
+   html_doc.search("#accom-detail-location").each do |element|
+    address = element.text.strip
+    # creating surfcamp address
+    surfcamp.address = address
+  end
+  ratings = []
+  html_doc.search("p.bolder.sh-orange span.bigger-font18").each do |element|
+    ratings << element.text.strip
+    # creating surfcamp rating
+    surfcamp.rating = ratings[0]
+  end
 
-#   # creating surfcamp capacity
-#   surfcamp.capacity = rand(6..50)
-#   # creating surfcamp price_per_night_per_person
-#   surfcamp.price_per_night_per_person = rand(30..70)
+  # creating surfcamp capacity
+  surfcamp.capacity = rand(6..50)
+  # creating surfcamp price_per_night_per_person
+  surfcamp.price_per_night_per_person = rand(30..70)
 
-#   surfcamp.save!
-#   s += 1
-#   puts "#{s}/#{surfcamp_total} created"
-# end
-# puts "Done creating surfcamps"
+  surfcamp.save!
+  s += 1
+  puts "#{s}/#{surfcamp_total} created"
+end
+puts "Done creating surfcamps"
 
 
 puts "Creating Discounted Prices"
