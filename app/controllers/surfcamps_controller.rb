800a86adfa8b85e0fa178ec0e4c2f605f302c5c5
Dima Muasses <dima.muasses7@gmail.com>
2017-08-24 07:03:11 -0400

    first steps weather parsing for the map

--- app/controllers/surfcamps_controller.rb
+++ app/controllers/surfcamps_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class SurfcampsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_surfcamp, only: [:show]
@@ -14,6 +17,28 @@ class SurfcampsController < ApplicationController
       marker.lng surfcamp.longitude
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
+
+    #Parsing Test#
+    all_weekly_weathers = []
+    @surfcamps.each do |surfcamp|
+      url = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=#{ENV['WEATHER_API']}&format=json&q=#{surfcamp.latitude},#{surfcamp.longitude}"
+      weather_serialized = open(url).read
+      weather = JSON.parse(weather_serialized)
+      weekly_weather_datas = []
+      weekly_weather_datas << surfcamp.name
+      weather['data']['weather'].each do |element|
+        date_weather_data = {}
+        date_weather_data[:date] = element['date']
+        date_weather_data[:max_temp] = element['maxtempC']
+        date_weather_data[:wind_speed] = element['hourly'][4]['windspeedKmph']
+        date_weather_data[:weather_description] = element['hourly'][4]['weatherDesc'].first['value']
+        date_weather_data[:wave] = element['hourly'][4]['swellPeriod_secs']
+        weekly_weather_datas << date_weather_data
+      end
+      weekly_weather = weekly_weather_datas
+      all_weekly_weathers << weekly_weather
+    end
+    @all_weathers = all_weekly_weathers
   end
 
   def surfcamp_params
