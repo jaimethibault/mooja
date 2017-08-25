24de181fae490b9006646a08ef80d3a3a233fea1
Thibault Jaime <jaime.thibault@gmail.com>
2017-08-25 13:19:18 +0200

    weather api working + green / red marker but slow

--- app/controllers/surfcamps_controller.rb
+++ app/controllers/surfcamps_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class SurfcampsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_surfcamp, only: [:show]
@@ -28,11 +31,33 @@ class SurfcampsController < ApplicationController
       marker.lng surfcamp.longitude
       marker.infowindow render_to_string(partial: "/surfcamps/map_box", locals: { surfcamp: surfcamp })
       marker.picture({
-        :url => "http://maps.google.com/mapfiles/ms/icons/red.png",
+        :url => "http://maps.google.com/mapfiles/ms/icons/#{marker_color(surfcamp)}.png",
         :width   => 40,
         :height  => 40
       })
     end
+
+    # # Parsing weather conditions for all surfcamps - FULL
+    # all_weekly_weathers = []
+    # @surfcamps.each do |surfcamp|
+    #   url = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=#{ENV['WEATHER_API']}&format=json&q=#{surfcamp.latitude},#{surfcamp.longitude}"
+    #   weather_serialized = open(url).read
+    #   weather = JSON.parse(weather_serialized)
+    #   weekly_weather_datas = []
+    #   weekly_weather_datas << surfcamp.id
+    #   weather['data']['weather'].each do |element|
+    #     date_weather_data = {}
+    #     date_weather_data[:date] = element['date']
+    #     date_weather_data[:max_temp] = element['maxtempC']
+    #     date_weather_data[:wind_speed] = element['hourly'][4]['windspeedKmph']
+    #     date_weather_data[:weather_description] = element['hourly'][4]['weatherDesc'].first['value']
+    #     date_weather_data[:wave] = element['hourly'][4]['swellPeriod_secs']
+    #     weekly_weather_datas << date_weather_data
+    #   end
+    #   weekly_weather = weekly_weather_datas
+    #   all_weekly_weathers << weekly_weather
+    # end
+    # @all_weathers = all_weekly_weathers
   end
 
   def show
@@ -40,6 +65,18 @@ class SurfcampsController < ApplicationController
   end
 
   private
+
+  def build_wave_period_hash
+    # Parsing weather conditions for all surfcamps - SIMPLIFIED
+    weekly_weather_datas = {}
+    @surfcamps.each do |surfcamp|
+      url = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=#{ENV['WEATHER_API']}&format=json&q=#{surfcamp.latitude},#{surfcamp.longitude}"
+      weather_serialized = open(url).read
+      weather = JSON.parse(weather_serialized)
+      weekly_weather_datas["#{surfcamp.id}"] = weather['data']['weather'].first['hourly'][4]['swellPeriod_secs']
+    end
+    @all_weathers = weekly_weather_datas
+  end
 
   def set_surfcamp
     @surfcamp = Surfcamp.find(params[:id])
@@ -55,6 +92,23 @@ class SurfcampsController < ApplicationController
     percentage_of_saving = 1 - (discounted_price).fdiv(original_price)
     # multiply by 100 and round it for display
     (percentage_of_saving * 100).round
+  end
+
+  def marker_color(surfcamp)
+    # ['green', 'red'].sample
+    # check si on a des infos météo pour ce surf camp
+    if build_wave_period_hash.key?(surfcamp.id.to_s)
+      # chercher le wave info pour le surfcamp
+      wave_period = build_wave_period_hash[surfcamp.id.to_s].to_f
+      # check s'il est inférieur à 10s >> red
+      if wave_period >= 10
+        return 'green'
+      else
+        return 'red'
+      end
+    else
+      return 'grey'
+    end
   end
 
   helper_method :percentage_of_savings
