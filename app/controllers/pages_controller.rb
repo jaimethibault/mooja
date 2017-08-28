c0e37764b246dbad4c56e2203905f16da0e1e423
Dima Muasses <dima.muasses7@gmail.com>
2017-08-28 13:45:20 -0400

    home not finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @surfcamps = []
+    Discount.all.each do |discount|
+      @surfcamps << discount.surfcamp
+    end
+    @surfcamps = @surfcamps.first(6)
+
   end
 end
